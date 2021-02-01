169d1240898a4df0b5f64eabb4aa223abbf7342a
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2021-01-31 22:31:55 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace controle_remoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
